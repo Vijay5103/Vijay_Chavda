e5252b1ad7fbc6358b7cc6deb0167e3601229fd0
Vijay Chavda <133740430+Vijay5103@users.noreply.github.com>
2024-05-11 20:04:41 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Python Developer . . ." , "RPA Developer . . ." , "Web Developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
